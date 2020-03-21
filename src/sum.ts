cabdbe1d18c4cff3de86adae4cb530a942b9e9c1
luckybo <luckybo0027@gmail.com>
2020-03-21 17:30:01 +0800

    feat: 两数之和算法

--- src/sum.ts
+++ src/sum.ts
@@ -1,4 +1,40 @@
-function sum(a, b) {
-  return a + b;
+/**
+ *
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+
+ 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+
+ 示例:
+
+ 给定 nums = [2, 7, 11, 15], target = 9
+
+ 因为 nums[0] + nums[1] = 2 + 7 = 9
+ 所以返回 [0, 1]
+
+ */
+
+/**
+ *
+ * @param nums
+ * @param target
+ */
+export default function twoSum(nums: number[], target) {
+  let first , second;
+  for (let i = 0; i < nums.length - 1; i++) {
+    let a = nums[i];
+    for (let j = i+ 1; j < nums.length; j++) {
+      let b = nums[j];
+      if(a + b == target){
+        first = i;
+        second = j;
+        break;
+      }
+    }
+
+    if(!!second){
+      break
+    }
+  }
+
+  return [first, second]
 }
-export default sum
--- test/sum.test.ts
+++ test/sum.test.ts
@@ -1,5 +1,43 @@
-import sum from '../src/sum'
+import twoSum from '../src/sum'
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+test('7 will return [0, 1]', () => {
+  const drink = jest.fn(twoSum);
+
+  drink([2, 5, 6, 7], 7);
+  expect(drink).toHaveReturnedWith([0, 1]);
+});
+
+test('8 will return [0, 2]', () => {
+  const drink = jest.fn(twoSum);
+
+  drink([2, 5, 6, 7], 8);
+  expect(drink).toHaveReturnedWith([0, 2]);
+});
+
+test('9 will return [0, 3]', () => {
+  const drink = jest.fn(twoSum);
+
+  drink([2, 5, 6, 7], 9);
+  expect(drink).toHaveReturnedWith([0, 3]);
+});
+
+test('11 will return [1, 2]', () => {
+  const drink = jest.fn(twoSum);
+
+  drink([2, 5, 6, 7], 11);
+  expect(drink).toHaveReturnedWith([1, 2]);
+});
+
+test('12 will return [1, 3]', () => {
+  const drink = jest.fn(twoSum);
+
+  drink([2, 5, 6, 7], 12);
+  expect(drink).toHaveReturnedWith([1, 3]);
+});
+
+test('13 will return [2, 3]', () => {
+  const drink = jest.fn(twoSum);
+
+  drink([2, 5, 6, 7], 13);
+  expect(drink).toHaveReturnedWith([2, 3]);
 });
